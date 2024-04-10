d39297951665491a4781b2af09a41e39fb36089f
Viivi Leivonen <viivi.leivonen@student.careeria.fi>
2024-04-10 15:25:19 +0300

    Muokattu etusivun näkymää. Lisätty usereiden haku etusivulle. Luotu User-controller.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,15 +1,49 @@
-﻿namespace TreeniApp
+﻿using System.Collections.ObjectModel;
+using TreeniApp.Models;
+using Newtonsoft;
+using Newtonsoft.Json;
+
+
+namespace TreeniApp
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        
+        // Muuttujan alustaminen
+        ObservableCollection<User> dataa = new ObservableCollection<User>();
 
         public MainPage()
         {
             InitializeComponent();
+            LoadDataFromRestAPI();
         }
 
-  
+        async void LoadDataFromRestAPI()
+        {
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri("https://treeniappbackend.azurewebsites.net");
+
+                string json = await client.GetStringAsync("/api/users");
+                var users = JsonConvert.DeserializeObject<User[]>(json);
+
+                dataa = new ObservableCollection<User>(users);
+                userList.ItemsSource = dataa;
+            }
+            catch (Exception e)
+            {
+                await DisplayAlert("Virhe", e.Message.ToString(), "SELVÄ!");
+            }
+        }
+
+
+
+
+
+
+
+
 
         private void Lisatieto_Clicked(object sender, EventArgs e)
         {
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.Maui.ApplicationModel.Permissions;
+
+namespace TreeniApp.Models
+{
+    public class User
+    {
+
+       
+      
+
+            public int UserId { get; set; }
+            public string Username { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+
+           
+       
+    }
+}
